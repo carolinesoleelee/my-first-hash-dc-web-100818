be0fc83509ef9a6a27df9bc87e729708b60e502d
Solee Lee <carolinesoleelee@gmail.com>
2018-09-27 18:33:23 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,14 +26,16 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping_manifest["oil paintings"] = 3
+   shipping_manifest["oil paintings"] = 3
 end
 
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
+                  "muskets" => 2,
+                  "gun powder" => 4,
                  }
   # add 2 muskets to the shipping_manifest hash below
 
